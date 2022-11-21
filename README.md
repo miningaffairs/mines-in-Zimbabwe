caedf3ef07ea115aaedbcbbe3024b6d880924a15
miningaffairs <miningaffairs083@gmail.com>
2022-11-21 09:30:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mines-in-Zimbabwe
+ Location of mines in Zimbawe
---
+++ data/mines_2.js
@@ -0,0 +1,15 @@
+var json_mines_2 = {
+"type": "FeatureCollection",
+"name": "mines_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Turk Mine", "LATITUDE": -19.71703, "LONGTITUDE": 28.78666 }, "geometry": { "type": "Point", "coordinates": [ 28.78666, -19.71703 ] } },
+{ "type": "Feature", "properties": { "NAME": "Casmyn Mine", "LATITUDE": -19.71671, "LONGTITUDE": 28.7961 }, "geometry": { "type": "Point", "coordinates": [ 28.7961, -19.71671 ] } },
+{ "type": "Feature", "properties": { "NAME": "Empress nickle refinery", "LATITUDE": -18.31603, "LONGTITUDE": 29.97383 }, "geometry": { "type": "Point", "coordinates": [ 29.97383, -18.31603 ] } },
+{ "type": "Feature", "properties": { "NAME": "Ngezi Plantinum", "LATITUDE": -1866507.0, "LONGTITUDE": 30.35601 }, "geometry": { "type": "Point", "coordinates": [ 30.35601, -1866507.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Trojan Nickle Mine", "LATITUDE": -17.33299, "LONGTITUDE": 31.28394 }, "geometry": { "type": "Point", "coordinates": [ 31.28394, -17.33299 ] } },
+{ "type": "Feature", "properties": { "NAME": "Unki Plantinum", "LATITUDE": -19.62247, "LONGTITUDE": 30.09483 }, "geometry": { "type": "Point", "coordinates": [ 30.09483, -19.62247 ] } },
+{ "type": "Feature", "properties": { "NAME": "Murowa Diamonds", "LATITUDE": -17.80667, "LONGTITUDE": 31.07641 }, "geometry": { "type": "Point", "coordinates": [ 31.07641, -17.80667 ] } },
+{ "type": "Feature", "properties": { "NAME": "Muriel Mine", "LATITUDE": -17.23958, "LONGTITUDE": 30.59873 }, "geometry": { "type": "Point", "coordinates": [ 30.59873, -17.23958 ] } }
+]
+}
